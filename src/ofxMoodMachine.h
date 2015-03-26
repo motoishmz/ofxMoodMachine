579f3aeacdb5609bdbf8d80f64f1ebd8e800a6e5
Motoi Shmizu <shimizu.motoi@gmail.com>
2015-03-27 01:53:15 +0900

    final commit

--- src/ofxMoodMachine.h
+++ src/ofxMoodMachine.h
@@ -13,13 +13,13 @@ public:
 		time_duration.g = 900 * 0.001;
 		time_duration.b = 1300 * 0.001;
 		
-		color_from.r = ofRandom(255);
-		color_from.g = ofRandom(255);
-		color_from.b = ofRandom(255);
+		color_from.r = ofRandom(255.f);
+		color_from.g = ofRandom(255.f);
+		color_from.b = ofRandom(255.f);
 		
-		color_to.r = ofRandom(255);
-		color_to.g = ofRandom(255);
-		color_to.b = ofRandom(255);
+		color_to.r = ofRandom(255.f);
+		color_to.g = ofRandom(255.f);
+		color_to.b = ofRandom(255.f);
 		
 		ofAddListener(ofEvents().update, this, &ofxMoodMachine::update);
 	}
@@ -32,7 +32,7 @@ public:
 	inline void start() { mood = true; }
 	inline void stop() { mood = false; }
 	inline void toggle() { mood ^= true; }
-	inline bool getMood() const { return mood; }
+	inline bool isMoody() const { return mood; }
 	
 private:
 	
@@ -47,7 +47,7 @@ private:
 	
 	void update(ofEventArgs &e)
 	{
-		if (getMood() == false)
+		if (isMoody() == false)
 			return;
 		
 		const float t = ofGetLastFrameTime();
@@ -59,21 +59,21 @@ private:
 		if (time_elapsed.r > time_duration.r)
 		{
 			color_from.r = color_to.r;
-			color_to.r = ofRandom(255);
+			color_to.r = ofRandom(255.f);
 			time_elapsed.r = 0.f;
 		}
 		
 		if (time_elapsed.g > time_duration.g)
 		{
 			color_from.g = color_to.g;
-			color_to.g = ofRandom(255);
+			color_to.g = ofRandom(255.f);
 			time_elapsed.g = 0.f;
 		}
 		
 		if (time_elapsed.b > time_duration.b)
 		{
 			color_from.b = color_to.b;
-			color_to.b = ofRandom(255);
+			color_to.b = ofRandom(255.f);
 			time_elapsed.b = 0.f;
 		}
 		
